8fa742d9ea2616f40d9c1d7c2198b0dd1de11f39
asgray <antgray89@gmail.com>
2020-03-28 18:29:48 -0700

    test

--- data_prep.py
+++ data_prep.py
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 # Anthony Gray
 # AS.410.734.81
 # Metagenomics Final Project
@@ -232,4 +233,240 @@ if args.extract:
     extract_gene_families(df, destination)
 
 if args.files:
+=======
+# Anthony Gray
+# AS.410.734.81
+# Metagenomics Final Project
+
+# This script uses the metadata file from the IBDMDB website to download and organize the associated data
+
+import os
+import csv
+import wget
+import pickle
+import tarfile
+import argparse
+import requests
+import pandas as pd
+from pathlib import Path
+
+# Method checks which of several urls on the IBDMDB site works for each sample set -----------------------------------
+def test_urls(row):
+    # retrieve sample accession
+    sample = row['External_ID']
+    data_type = row['data_type']
+    filename =  sample + '_humann2.tar.bz2'
+    # each URL base links to a different project section on the site
+    url1 = 'https://ibdmdb.org/tunnel/products/HMP2/WGS/1818/' + filename
+    url2 = 'https://ibdmdb.org/tunnel/products/SwedishTwins/WGS/1441/' + filename
+    url3 = 'https://ibdmdb.org/tunnel/products/HMP2_Pilot/WGS/1644/' + filename
+    url4 = 'https://ibdmdb.org/tunnel/products/HMP2_Pilot/MTX/1524/' + filename
+    url5 = 'https://ibdmdb.org/tunnel/products/HMP2/MTX/1750/' + filename
+    # duplicate IDs require the right data download
+    genome_urls = [url1, url2, url3]
+    trancript_urls = [url4, url5]
+    urls = genome_urls if data_type == 'metagenomics' else trancript_urls
+    # test if each url is good, return the one that works
+    for url in urls:
+        # test if each url is good, return the one that works
+        res = requests.head(url)
+        if res.status_code == 200:
+            print(url + ' works')
+            return url
+        else:
+            print(filename + ' bad url')
+# end test_urls() ----------------------------------------------------------------------------------------------------
+
+# Method uses test_urls() to fill in URL for each sample in the metadata spreadsheet ---------------------------------
+def assign_urls(df):
+    df['url'] = None
+    df['url'] = df.apply(test_urls, axis=1)
+# end assign_urls() --------------------------------------------------------------------------------------------------
+
+# Method downloads Tarfile for each sample based on the URLS filled into the metadata spreadsheet --------------------
+def download_samples(df, destination):
+    # test if file has already been downloaded
+    already_downloaded = os.listdir(destination)
+    i = 1
+    for row in df.itertuples():
+        url = row[4]
+        sample = row[1]
+        data_type = row[2]
+        dt = 'DNA' if data_type == 'metagenomics' else 'RNA'
+        file_name = sample + '_' + dt + '_' + 'humann2.tar.bz2'
+        file_path = destination + file_name
+        # download samples via associated URL
+        if file_name not in already_downloaded:
+            try:
+                print('Downloading ' + file_name)
+                wget.download(url, file_path)
+                print(f'\n sample {i} downloaded successfully \n')
+            except Exception as e:
+                print('\n' + file_name + ' could not be downloaded: ' + str(e) + '\n')
+        else:
+            print(file_name + ' already downloaded')
+        i += 1
+# end download_samples() ----------------------------------------------------------------------------------------------
+
+# Mehod retrieves gene family composition file from tarfile, saves total composition and composition by taxa in separate files
+def extract_gene_families(df, destination):
+    # make new directory
+    genefamilies_folder = destination + 'GeneFamilies'
+    # check extracted files that already exist 
+    already_extracted_dna = os.listdir(genefamilies_folder + '/DNA')
+    already_extracted_rna = os.listdir(genefamilies_folder + '/RNA')
+    already_extracted = already_extracted_dna + already_extracted_rna
+    for row in df.itertuples():
+        sample = row[1]
+        data_type = row[2]
+        dt = 'DNA' if data_type == 'metagenomics' else 'RNA'
+        tarfile_name = sample + '_' + dt + '_humann2.tar.bz2'
+        print(tarfile_name)
+        genefamilies_filename = sample + '_genefamilies.tsv'
+        if sample + '_total.csv' not in already_extracted:
+            print('Extracting gene families from ' + tarfile_name)
+            try:
+                # extract gene family file
+                tar = tarfile.open(Path(destination + tarfile_name), 'r:bz2') 
+                f = tar.extractfile(genefamilies_filename)
+                content = f.readlines()
+                # save each line in one of two lists
+                total_families = []
+                families_by_tax = []
+                for line in content:
+                    line = line.decode().strip()
+                    # '#' is the header line
+                    if '|' not in line and '#' not in line:
+                        line = line.split('\t')
+                        total_families.append(line)
+                    # '|' is used to separate the gene family and taxonomy information
+                    elif '|' in line and '#' not in line:
+                        line = line.split('\t')
+                        families_by_tax.append(line)
+                    else:
+                        pass
+                # make dataframe of each list of lines
+                total_df = pd.DataFrame(total_families)
+                tax_df = pd.DataFrame(families_by_tax)
+                # save dataframes as CSVs
+                total_df.to_csv(genefamilies_folder + '/'+dt+'/' + sample + '_' + dt + '_total.csv', index=False, header=False)
+                tax_df.to_csv(genefamilies_folder + '/'+dt+'/' + sample + '_' + dt +'_by_taxa.csv', index=False, header=False)
+            except Exception as e:
+                print(tarfile_name + ' could not be extracted: ' + str(e) + '\n')
+        else:
+            print('Gene families already extracted from ' + tarfile_name)
+# end extract_gene_families() -----------------------------------------------------------------------------------------------------
+
+# Method uses metadata file, selected attribute file, and gene family files to build a dataset ------------------------------------
+# each sample is a row, each gene family is a column
+def collate_datasets(df, destination, files_path, set_flag, pickle_file):
+    # retrieve list of selected attributes
+    with open(pickle_file, 'rb') as f:
+        attrs = pickle.load(f)
+    # parse flag string to get data type and subset
+    data_type = set_flag[:3].upper()
+    attribute_type = set_flag[-3:]
+
+    print(data_type, attribute_type)
+    out_file_name = set_flag + '_df.csv'
+    instances = []
+    # collect extracted files 
+    file_list = os.listdir(files_path)
+    i = 1
+    for row in df.itertuples():
+        # extract metadata
+        sample, sample_type, diagnosis = row[1], row[2], row[3]
+        # only choose targeted metadata 
+        if (sample_type == 'metagenomics' and data_type == 'DNA') or (sample_type == 'metatranscriptomics' and data_type == 'RNA'):
+            print('Adding ' + sample)
+            # retreive list of files associated with sample
+            sample_files = [file for file in file_list if file.startswith(sample) and data_type in file and attribute_type in file]
+
+            for file in sample_files:
+                # build dictionary of sample attributes
+                instance = {'sample': sample, 'diagnosis': diagnosis}
+                with open(files_path + file) as f:
+                    readCSV = csv.reader(f, delimiter = ',')
+                    for row in readCSV:
+                            attr = row[0]
+                            if attr in attrs:
+                                val = float(row[1])
+                                # round extremely small values down to 0
+                                instance[attr] = val if val >= 0.00001 else 0.0
+                instances.append(instance)
+            print(f'sample {i} added successfully')
+            i += 1
+    # convert list of dictionaries to dataframe, fill missing values with 0
+    print('Converting Dataframe...')
+    final_df = pd.DataFrame.from_dict(instances)
+    final_df = final_df.fillna(0.0)
+    # move diagnosis to last column
+    cols = list(final_df.columns)
+    cols = cols[-1:] + cols[:-1]
+    final_df = final_df[cols]
+    print(final_df.shape)
+
+    # remove columns with one value
+    nunique = final_df.apply(pd.Series.nunique)
+    cols_to_drop = nunique[nunique == 1].index
+    final_df = final_df.drop(cols_to_drop, axis=1)
+    # print(final_df)
+    print('Writing Dataframe to file')
+    # save as data file
+    final_df.to_csv(destination + out_file_name, index=False)
+# end collate_datasets() ----------------------------------------------------------------------------------------------------------
+
+# *********************************************************************************************************************************
+# Begin Script
+# *********************************************************************************************************************************
+# script args
+parser = argparse.ArgumentParser(description='Test')
+parser.add_argument('-o', '--output_folder', type=str, required=True, help='Output Destination')
+parser.add_argument('-d', '--data', type=str, required=True, help='Import dataset path')
+parser.add_argument('-u', '--url', type=bool, required=False, help='Option for url management')
+parser.add_argument('-g', '--get', type=bool, required=False, help='Option to download data')
+parser.add_argument('-s', '--save', type=bool, required=False, help='Option to save dataset with urls')
+parser.add_argument('-e', '--extract', type=bool, required=False, help='Option to extract files from tarfiles')
+parser.add_argument('-f', '--files', type=str, required=False, help='Path to sample files')
+parser.add_argument('-x', '--set_flag', type=str, required=False, help='Flag to indicate which type of data to collate: dna_tax, dna_tot, rna_tax, rna_tot')
+parser.add_argument('-p', '--pickle_file', type=str, required=False, help='path to pickled attribute list')
+
+args = parser.parse_args()
+data_path = args.data
+destination = args.output_folder
+# windows path parsing adds '"' to argument, need to trim to use filepath string
+# FIX ME ********************************************
+if args.extract:
+    destination = destination[:-1] + '\\'
+# DON'T KNOW WHY THIS IS NEEDED
+# ***************************************************
+files_path = args.files
+pickle_file = args.pickle_file
+
+# prevent invalid flags
+set_flag = args.set_flag
+possible_set_flags = ['dna_tax', 'dna_tot', 'rna_tax', 'rna_tot']
+if set_flag and set_flag not in possible_set_flags:
+    print('Possible set flags are: gen_tax, gen_tot, tran_tax, tran_tot')
+    quit()
+
+# import metadata file
+df = pd.read_csv(data_path)
+
+# run methods based on arg flags
+if args.url:
+    assign_urls(df)
+
+if args.save:
+    print('Saving dataset with URLs')
+    df.to_csv(data_path[:-4] + '_url.csv', index = False)
+
+if args.get:
+    download_samples(df, destination)
+
+if args.extract:
+    extract_gene_families(df, destination)
+
+if args.files:
+>>>>>>> d11fed841e9ab9caa986f2a85b287800468216d2
     collate_datasets(df, destination, files_path, set_flag, pickle_file)
--- data_process.py
+++ data_process.py
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 # Anthony Gray
 # AS.410.734.81
 # Metagenomics Final Project
@@ -146,4 +147,154 @@ if args.pca:
     do_PCA(df, output)
 
 if args.configure:
+=======
+# Anthony Gray
+# AS.410.734.81
+# Metagenomics Final Project
+
+# This script operates on data files to produce modified data files, visualizations, and quality control
+
+import csv
+import argparse
+import numpy as np
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+from sklearn.decomposition import PCA
+import scipy.cluster.hierarchy as sch
+from sklearn.preprocessing import StandardScaler
+
+# method applies standard scaling to dataframe and saves to a new file -------------------------------------------------------------------------------
+def standardize_data(df, output):
+    samps = df['sample']
+    diags = df['diagnosis']
+    features = [col for col in df.columns.values if col not in ['sample', 'diagnosis']]
+    x = StandardScaler().fit_transform(df[features])
+    df_std = pd.DataFrame(x, columns=features)
+    df_std['sample'] = samps
+    df_std['diagnosis'] = diags
+    df_std.set_index('sample', inplace=True)
+    df_std.to_csv(output)
+# end standardize_data() -----------------------------------------------------------------------------------------------------------------------------
+
+# method outputs scree plot for dataset --------------------------------------------------------------------------------------------------------------
+def plot_scree(df ,output, data_set):
+    features = df.columns[1:-1]
+    pca = PCA().fit(df[features])
+    sns.lineplot(data = pca.explained_variance_ratio_)
+    plt.title(data_set + ' Scree Plot')
+    plt.xlabel('PC')
+    plt.ylabel('Explaination of Variance')
+    plt.savefig(output)
+# end plot_scree() -----------------------------------------------------------------------------------------------------------------------------------
+
+# method performs hierarchical clustering on dataset and outputs dendrogram --------------------------------------------------------------------------
+def plot_dendrogram(df, output, data_set):
+    label_list = list(df['diagnosis'])
+    df.drop(['sample','diagnosis'], inplace=True, axis=1)
+    X = df.values
+    dendrogram = sch.dendrogram(sch.linkage(X, method='ward'), orientation='left', labels=label_list)
+    plt.title(data_set + ' Dendrogram')
+    plt.ylabel('Samples')
+    plt.xlabel('Euclidean distances')
+    plt.show()
+# end plot_dendrogram() ------------------------------------------------------------------------------------------------------------------------------
+
+# method plots a grouped bar chart of the abundances of gene families in each dataset ---------------------------------------------------------------- 
+def plot_features(df,df2, output):
+    # get gene families present in RNA and DNA datasets
+    rna_attrs = list(df.columns[1:-1])
+    dna_attrs = list(df2.columns[1:-1])
+    # find unique gene families
+    attrs = set(rna_attrs + dna_attrs)
+    # get counts of number of samples with each gene family present
+    rna_counts = df[df[rna_attrs] > 0].count().reset_index()
+    dna_counts = df2[df2[dna_attrs] > 0].count().reset_index()
+    rna_counts.columns = ['Gene Family', 'RNA Count']
+    dna_counts.columns = ['Gene Family', 'DNA Count']
+    # create dataframe with rows for each unique family
+    merged = pd.DataFrame()
+    merged['Gene Family'] = list(attrs)
+    # join RNA and DNA counts on gene family names
+    merged = pd.merge(merged, rna_counts,  on='Gene Family')
+    merged = pd.merge(merged, dna_counts,  on='Gene Family')
+    # fill missing data with 0
+    merged.fillna(0, inplace=True)
+    # plot bar chart
+    merged.set_index('Gene Family')[['RNA Count', 'DNA Count']].plot(kind='bar', figsize=(14, 10))
+    plt.title('Percentile Trimmed Gene Families Sample Occurrence')
+    plt.xticks([])
+    plt.ylabel('Counts')
+    plt.savefig(output)
+# end plot_features() ------------------------------------------------------------------------------------------------------------------------------
+
+# method performs PCA on dataset, retaining 95% of it's variability and saving to file -------------------------------------------------------------
+def do_PCA(df, output):
+    samps = df['sample']
+    df.set_index('sample', inplace=True)
+    diags = df['diagnosis']
+    pca = PCA(0.95)
+    features = df.columns[:-1]
+    pca.fit(df[features])
+    df_pca = pca.transform(df[features])
+    df_pca = pd.DataFrame(df_pca)
+    df_pca.set_index(samps, inplace=True)
+    df_pca['diagnosis'] = diags
+    df_pca.to_csv(output)
+    print(df_pca.shape)
+# do_PCA() -----------------------------------------------------------------------------------------------------------------------------------------
+
+# method ensures that dataframe is ready for use by classification methods -------------------------------------------------------------------------
+def configure_df(df, data):
+    # set samples as index
+    df.set_index('sample', inplace=True)
+    # make diagnosis last column
+    df = df[[c for c in df if c not in ['diagnosis']] + ['diagnosis']]
+    print(df.head())
+    # pickle
+    df.to_pickle(data[:-4] + '.pkl')
+# end configure_df() --------------------------------------------------------------------------------------------------------------------------------
+
+# *********************************************************************************************************************************
+# Begin Script
+# *********************************************************************************************************************************
+# script args
+parser = argparse.ArgumentParser(description='Test')
+parser.add_argument('-d', '--data', type=str, required=True, help='Import data path')
+parser.add_argument('-d2', '--data2', type=str, required=False, help='2nd Import data path')
+parser.add_argument('-o', '--output', type=str, required=True, help='output file path')
+parser.add_argument('-ds', '--data_set', type=str, required=False, default='', help='Dataset, either RNA or DNA')
+parser.add_argument('-s', '--standardize', type=bool, required=False, help='save standardized version of dataset')
+parser.add_argument('-ps', '--scree', type=bool, required=False, help='output scree plot for dataset')
+parser.add_argument('-hc', '--cluster', type=bool, required=False, help='output dendrogram')
+parser.add_argument('-f', '--features', type=bool, required=False, help='generate plot of feature distribution')
+parser.add_argument('-pca', '--pca', type=bool, required=False, help='convert dataframe to PCA dataset')
+parser.add_argument('-c', '--configure', type=bool, required=False, help='arrange df to work with classification methods')
+
+args = parser.parse_args()
+output = args.output
+data = args.data
+data2 = args.data2
+data_set = 'Metatranscriptome' if args.data_set == 'RNA' else 'Metagenome'
+
+df = pd.read_csv(data)
+
+if args.standardize:
+    standardize_data(df, output)
+
+if args.scree:
+    plot_scree(df, output, data_set)
+
+if args.cluster:
+    plot_dendrogram(df, output, data_set)
+
+if args.features:
+    df2 = pd.read_csv(data2)
+    plot_features(df,df2,output)
+
+if args.pca:
+    do_PCA(df, output)
+
+if args.configure:
+>>>>>>> d11fed841e9ab9caa986f2a85b287800468216d2
     configure_df(df, data)
